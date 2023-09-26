9fe111258c2a096bfa3504775812c507ce30bdef
rajul786dixit <rajul786dixit@gmail.com>
2023-09-27 04:59:10 +0530

    progress bar added

---
+++ src/Components/CustomProgressBar.tsx
@@ -0,0 +1,20 @@
+import { Box, LinearProgress, LinearProgressProps, Typography } from '@mui/material';
+import React from 'react'
+
+const CustomProgressBar = (props: LinearProgressProps & { value: number }) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+     <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">Your progress - {`${Math.round(
+            props.value,
+        )}%`}</Typography>
+      </Box>   
+      <Box sx={{ width: '30%', ml: 1}}>
+        <LinearProgress sx={{height: '16px', borderRadius: '8px'}} variant="determinate" {...props} />
+      </Box>
+      
+    </Box>
+  );
+}
+
+export default CustomProgressBar
--- src/Components/Questionnaire.tsx
+++ src/Components/Questionnaire.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react'
-import { Stack, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import { Box, Stack, Typography } from '@mui/material';
 import { QuestionnaireInfoText } from '../utils/constants';
 import { useAppDispatch, useAppSelector } from '../AppState/hooks';
 import { fetchQuestions } from '../AppState/questionaireSlice';
+import CustomProgressBar from './CustomProgressBar';
 
 const Questionnaire = (props: {username: string}) => {
   const dispatch = useAppDispatch(),
   questions = useAppSelector(state => state.question.questions),
+  [progress, setProgress] = useState(0),
   { username } = props;
   let isRan = false
 
@@ -17,13 +19,26 @@ const Questionnaire = (props: {username: string}) => {
     isRan = true
     return
   }, [username])
+
+  useEffect(() => {
+    // const timer = setInterval(() => {
+    //   setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
+    // }, 800);
+    return () => {
+      // clearInterval(timer);
+    };
+  }, [])
  
   return (
     <Stack p={4} m={4} spacing={2} sx={{boxSizing: 'border-box'}}>
       <Typography variant='body1'>{QuestionnaireInfoText.first}</Typography> 
       <Typography variant='body1'>{QuestionnaireInfoText.second}</Typography> 
       {
-        (questions && questions.length > 0 ) ? 'hi' : 'no'
+        (questions && questions.length > 0 ) ? <Box>
+          <Stack>
+            <CustomProgressBar value={progress} />
+          </Stack>
+        </Box> : <></>
       } 
     </Stack>
   )
