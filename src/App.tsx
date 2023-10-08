11d167eca4aa651c45b94e67ab86280560383649
rajul786dixit <rajul786dixit@gmail.com>
2023-10-08 23:14:33 +0530

    integration with express app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
+import axios from "axios"
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 const LazyDashboard = lazy(() => import('./Pages/Dashboard/Dashboard'))
 
 const App = () => {
+  useEffect(() => {
+    axios.get("http://localhost:9000/careerPath")
+        .then(res => console.log(res));
+  }, [])
   return(
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={
-        <Suspense fallback='loading...'>
-          <LazyDashboard />
-        </Suspense>
-      } />
-     </Routes>
+    // <Routes>
+    //   <Route path='/' element={<Home />} />
+    //   <Route path='/dashboard' element={
+    //     <Suspense fallback='loading...'>
+    //       <LazyDashboard />
+    //     </Suspense>
+    //   } />
+    //  </Routes>
+    <></>
   )
 }
 
